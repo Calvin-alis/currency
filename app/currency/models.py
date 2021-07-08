a86442455ab16d105f30a7f5ee9d2160c963723a
Alexsander Demiurg <alis.demiurg@gmail.com>
2021-07-08 23:31:32 +0300

    Add files

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+#id устанавливать нам не нужно django сам его установит
+class ContactUs(models.Model):
+    email_form = models.EmailField() #  установлин автоматический валидатор
+    subject = models.CharField(max_length=25) #  удобно устанавливать ограничения
+    message = models.TextField()
+
+
