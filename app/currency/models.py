afdb702b27258d4b4fbe4652d24fab85411afb43
Alexsander Demiurg <alis.demiurg@gmail.com>
2021-07-11 11:25:07 +0300

    Update models file

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
-# Create your models here.
 
-#id устанавливать нам не нужно django сам его установит
+# было на уроки (хороший пример)
+class Rate(models.Model):
+    sale  = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2)
+    created = models.DateTimeField(auto_now_add=True)
+    source = models.CharField(max_length=32)
+    type  = models.CharField(max_length=3)
+
+
+# id устанавливать нам не нужно django сам его установит
 class ContactUs(models.Model):
-    email_form = models.EmailField() #  установлин автоматический валидатор
-    subject = models.CharField(max_length=25) #  удобно устанавливать ограничения
+    #  установлин автоматический валидатор
+    email_form = models.EmailField()
+    #  удобно устанавливать ограничения
+    subject = models.CharField(max_length=25)
     message = models.TextField()
-
-
